54f73a2299331ad7c4f9e71492b6b3ed68b757fa
Metallium96 <giamma.barbato@gmail.com>
2022-09-07 12:10:35 +0200

    Commit disegno query plan init

--- DistributedQueryUI/utils.py
+++ DistributedQueryUI/utils.py
@@ -1,4 +1,6 @@
 #Modulo delle Utilities (funzioni ad uso comune)
+import os
+import platform
 
 ope_name = {
 		"base" : "",
@@ -56,3 +58,102 @@ def parseUint(str):
 		return int(str)
 	except:
 		return -1
+
+def draw_tree(qp, names_set, first):
+	#Step 4: Generazione albero
+	lista_ocd = qp.get_ocd()
+	lista_asc = qp.get_asc()
+	html_albero = ""
+	html_ls_nodi = ""
+	html_kes = ""
+
+	for i in range(1, qp.get_num_nodi()+1):
+	
+		html_ls_nodi += (", " if i != 1 else "") + "node" + str(i)
+
+		html_nodo = ""
+		nodo = qp.get_nodo(i)
+		vp, ve, ip, ie, eq, cand, assegn, operazione, attributi, operandi, dett_op = nodo.get_profilo()
+		html_nodo += "node" + str(i) + " = {\n"
+		html_nodo += "parent: node" + str(nodo.id_padre) + "," if i != 1 else ""
+		html_nodo += "innerHTML : \""
+	
+		if not first:
+			for ocd in lista_ocd:
+				if i == ocd["figlio"] and ocd["tipo_op"] == "C":
+					html_nodo += "<div class='box_up'><span class='enc'>" + ", ".join(list(ocd["adc"])) + "</span></div><br/>"
+	
+			html_nodo += "<div class='box_left'>"
+
+			if operazione != "base" and not qp.is_proj_after_base(i):
+				cand_list = "<span class='as'>" + assegn + "</span>, "
+				cand.remove(assegn)
+				cand_list +=  ", ".join(list(cand))
+				html_nodo += cand_list + "</div>"
+
+			else:
+				html_nodo += "<span class='as'>" + assegn + "</span></div>"
+
+		html_nodo += "<div class='box_center " + (" leaf'" if operazione == "base" else "'") + "><p class='op'>"
+		#Parti di output generate in base al tipo di operazione
+		if operazione == "gby":
+			html_nodo += dett_op + " on " + str(operandi).replace("'", "") + ", grouping"
+		else:
+			html_nodo += ope_name[operazione]
+
+		if operazione != "base":
+			html_nodo += " on " + str(attributi).replace("'", "")
+
+		else:
+			html_nodo += names_set[list(operandi)[0]] + " (" + ", ".join(list(attributi)) + ")"
+	
+		html_nodo += "</p></div>"
+		
+		if not first:
+			html_nodo += "<div class='arrow_left'></div><div class='cont_right'><div class='box_right'>"
+			html_nodo += "<i>v</i>: " + ", ".join(list(vp)) + (" <span class='enc_att'>" + ", ".join(list(ve)) + "</span>" if len(ve) > 0 else "") + "<br/>"
+			html_nodo += "<i>i</i>: " + ", ".join(list(ip)) + (" <span class='enc_att'>" + ", ".join(list(ie)) + "</span>" if len(ie) > 0 else "") + "<br/>"
+			html_nodo += "<i>eq</i>: "
+			tmp_list = []
+			for eq_set in list(eq):
+				tmp_list.append("{" + ", ".join(eq_set) + "}")
+			html_nodo += ",".join(tmp_list)
+			html_nodo += "</div></div>"
+
+			#Parti di output generate in base all'eventuale cifratura
+			for ocd in lista_ocd:
+				if i == ocd["padre"] and ocd["tipo_op"] == "D":
+					html_nodo += "<br/><div class='box_down'><span class='enc'>" + ", ".join(ocd["adc"]) + "</span></div>"
+
+		html_nodo += "\"\n};\n\n"
+		html_albero += html_nodo
+
+	if not first:
+		html_kes += "<div class=\"kes_cont\" id=\"kes_separator\"><span class=\"kes\"><b>Key Encryption Sets</b><ul>"
+		for asc in lista_asc:
+			html_kes += "<li>" + str(asc["kes"]).replace("'", "") + " - Key to be given to " + str(asc["sogg"]).replace("'", "") + "</li>\n"
+		html_kes += "</ul></span></div>"
+
+	#Salvataggio dei dati nell'html finale
+	f_base_html = open('./base_html/index.html',mode='r')
+	base_html = f_base_html.read()
+	f_base_html.close()
+
+	end_html = base_html.replace("{{{nodes}}}", html_albero).replace("{{{n_list}}}", html_ls_nodi).replace("{{{kes}}}", html_kes)
+
+	html_name = ""
+	if first:
+		html_name = "qp_init.html"
+
+	else:
+		html_name = "qp_end.html"
+
+	out_html = open("./output/" + html_name, "w")
+	out_html.write(end_html)
+	out_html.close()
+
+	print("\nOpening file...")
+	if platform.system() == "Darwin":
+		os.system("open ./output/" + html_name)
+	else:
+		os.system("start ./output/" + html_name)
